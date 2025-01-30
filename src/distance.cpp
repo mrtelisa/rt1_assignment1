638efa08ce7f8cdbe95ee1226df5d2a020341de1
Elisa <s6504193@studenti.unige.it>
2025-01-30 08:44:24 +0000

    Es2 exam commit

--- src/distance.cpp
+++ src/distance.cpp
@@ -3,6 +3,8 @@
 #include "geometry_msgs/Twist.h"
 #include "std_msgs/String.h"
 #include "std_msgs/Float32.h"
+#include "turtlesim/Spawn.h"
+#include "turtlesim/Kill.h"
 #include <string>
 #include <iostream>
 #include <cmath>
@@ -14,6 +16,10 @@
 ros::Publisher pub_turtle1;
 ros::Publisher pub_turtle2;
 ros::Publisher dist_pub;
+
+ros::ServiceClient kill_client1;
+ros::ServiceClient kill_client2;
+ros::ServiceClient spawn_client;
 
 geometry_msgs::Twist stop_msg{};
 
@@ -32,20 +38,63 @@ void handle_stopping(){
     if(stop){
         ROS_INFO("Stopping the turtles due to collision.");
         stop =  false;
-        pub_turtle1.publish(stop_msg);
-        pub_turtle2.publish(stop_msg);
+        teleport_turtle(1);
+        teleport_turtle(2);
     }
 
     if(bord_dist1){
         ROS_INFO("Stopping turtle1 due to collision with the border.");
         bord_dist1 = false;
+        teleport_turtle(1);
     }
     
     if(bord_dist2){
         ROS_INFO("Stopping turtle2 due to collision with the border.");
         bord_dist2 = false;
+        teleport_turtle(2);
     }
 }
+
+bool safe_pos(float x, float y){
+    if (x < 2 || x > 8 || y < 2 || y > 8){
+        return false;
+    }
+    
+    if (sqrt(pow(x - x_turtle1[0], 2) + pow(y - y_turtle1[0], 2)) < 2 ||
+        sqrt(pow(x - x_turtle2[0], 2) + pow(y - y_turtle2[0], 2)) < 2) {
+        return false;
+    }
+    return true;
+}
+
+void teleport_turtle(int turtle_id){
+    turtlesim::Kill kill;
+    turtlesim::Spawn spawn;
+    float x, y;
+    do {
+        x = rand() % 8 + 1;
+        y = rand() % 8 + 1;
+    } while (!safe_pos(x,y));
+
+    spawn.request.x = x;
+    spawn.request.y = y;
+    spawn.request.theta = 0.0;
+
+    if(turtle_id == 1){
+        kill.request.name = "turtle1";
+        kill_client1.call(kill);
+        spawn.request.name = "turtle1";
+    }
+
+    if(turtle_id == 2){
+        kill.request.name = "turtle2";
+        kill_client1.call(kill);
+        spawn.request.name = "turtle2";
+    }
+
+    spawn_client.call(spawn);
+}
+
 
 void set_vel1(const turtlesim::Pose::ConstPtr& msg){
     x_turtle1[1] = x_turtle1[0];
@@ -76,6 +125,10 @@ int main (int argc, char **argv){
 
     ros::Subscriber sub_turtle1 = nh.subscribe("/turtle1/pose", 10, set_vel1);
     ros::Subscriber sub_turtle2 = nh.subscribe("/turtle2/pose", 10, set_vel2);
+
+    kill_client1 = nh.serviceClient<turtlesim::Kill>("/kill");
+    kill_client2 = nh.serviceClient<turtlesim::Kill>("/kill");
+    spawn_client = nh.serviceClient<turtlesim::Spawn>("/spawn");
 
     ros::Rate rate(10);	
     
